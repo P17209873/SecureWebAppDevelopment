3447e08302ac26a99c6aa3aece36f9e887936610
Sam Spencer-McFadden <P17206266@GWHXX581LIN009W>
2019-11-18 13:35:14 +0000

    Created Login and Registration pages. Added Border format to CSS

--- private/bootstrap.php
+++ private/bootstrap.php
@@ -5,3 +5,20 @@
  * Date: 08/11/2019
  * Time: 00:37
  */
+session_start();
+
+require 'vendor/autoload.php';
+
+$settings = require __DIR__ . '/app/settings/php';
+
+$container =  new \Slim\Container($settings);
+
+require __DIR__ . '/app/dependencies.php';
+
+$app = new \Slim\App($container);
+
+require __DIR__ . '/app/routes.php';
+
+$app->run();
+
+session_regenerate_id(true);
---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matt
+ * Date: 08/11/2019
+ * Time: 00:36
+ */
+
+require 'SecureWebAppCoursework/bootstrap.php';
