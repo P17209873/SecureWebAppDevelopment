31f0eaa34f207a41efdc4a20b43f0c46c583dea7
Matt <contact@matttrim.com>
2019-12-14 00:26:57 +0000

    Updated files to implement login and registration

--- private/app/src/DatabaseWrapper.php
+++ private/app/src/DatabaseWrapper.php
@@ -96,10 +96,6 @@ class DatabaseWrapper
         $this->errors['db_error'] = false;
         $query_parameters = $params;
 
-        var_dump($query_string);
-
-        var_dump($query_parameters);
-
         try
         {
             $this->prepared_statement = $this->db_handle->prepare($query_string);
